8ab74d34f7ad8fd7eda45cac5276645250ba65c9
Hobson Lane <github@totalgood.com>
2018-02-17 21:53:14 -0800

    half of doctests in pattern.py pass (half fail)

--- src/aichat/pattern.py
+++ src/aichat/pattern.py
@@ -9,32 +9,52 @@ except ImportError:
         def getone(s):
             return s
 
+REGEX_ALIASES = {
+    r'\w': r'a-zA-Z_',
+    r'\s': r'\t\r\n\ ',
+    # r'\W': r'^a-zA-Z_',
+    # r'\S': r'^\t\r\n\ ',
+    }
 
 logger = logging.getLogger(__name__)
 
 
-def expand_pattern(pattern):
-    """ Expand regular expression character class aliases like \w and \s for FSM generators like exrex
+def expand_charclass(patt, alias=r'\w', expansion=None):
+    r""" Translate a single character class shortcut into its full explicit square-bracket form
 
-    >>> expand_pattern(r'Hello [\w\s]*')
-    'Hello [a-zA-Z_\n\r\t ]*'
+    >>> expand_charclass(patt=r'[\w\r\s]*', alias=r'\w', expansion=r'a-zA-Z_')
+    '[a-zA-Z_\\r\\s]*'
+
+    FIXME:
+      Fails on multiple square-bracketed character classes
+      Fails on square-bracketed character classes that include or exclude literal square brackets
+    """
+    # patt = patt.replace('[ ]', r'\ ')
+    if expansion is None:
+        expansion = REGEX_ALIASES[alias]
+    patt = patt.replace(alias, '[' + expansion + ']')
+    # clean up any nested square-brackets
+    patt = regex.sub(r'(.*)\[([^\[\]]*)\[' + expansion + r'\](.*)\](.*)', r'\1[\2' + expansion + r'\3]\4', patt)
+    return patt
+
+
+def expand_pattern(patt):
+    r""" Expand regular expression character class aliases like \w and \s for FSM generators like exrex
+
+    >>> expand_pattern(r'Hello[ ][\w\s]*')
+    'Hello[ ][a-zA-Z_\\t\\r\\n\\ ]*'
 
     FIXME:
       Correctly expand all character classes, not just \s and \w
     """
-    pattern = regex.sub(r'(\[[^\]]*)\w([^\]]\])', pattern, r'\1a-zA-Z_\2')
-    pattern = regex.sub(r'\w', pattern, r'[a-zA-Z_]')
-    # FIXME: pattern = regex.sub(r'(\[[^\]]*)\W([^\]]\])', pattern, r'\1a-zA-Z_\2')
-    pattern = regex.sub(r'\W', pattern, r'[^a-zA-Z_]')
-    pattern = regex.sub(r'(\[[^\]]*)\s([^\]]\])', pattern, r'\1\n\r\t \2')
-    pattern = regex.sub(r'\s', pattern, r'[\n\r\t ]')
-    # FIXME: pattern = regex.sub(r'(\[[^\]]*)\s([^\]]\])', pattern, r'\1\n\r\t \2')
-    pattern = regex.sub(r'\S', pattern, r'[^\n\r\t ]')
-    return pattern
+    # patt = patt.replace('[ ]', r'\ ')
+    for alias in REGEX_ALIASES:
+        patt = expand_charclass(patt, alias)
+    return patt
 
 
-def truncate_pattern(pattern, star_len=10, plus_len=None):
-    """ Replace Kleine '*' and '+' regex quantifiers with {star_len} so that FSM generators like exrex terminate
+def truncate_pattern(patt, star_len=10, plus_len=None):
+    r""" Replace Kleine '*' and '+' regex quantifiers with {star_len} so that FSM generators like exrex terminate
 
     >>> exrex_pattern(r'(Hello)+ [\w\s]*')
     '(Hello){10} [\w\s]{10}'
@@ -55,71 +75,82 @@ def truncate_pattern(pattern, star_len=10, plus_len=None):
             logger.warn("That's a REALY BIG Kleine plus (+)." +
                         "Do you really want {}-char strings in your language?".format(plus_len))
     star_len, plus_len = str(star_len), str(plus_len)
-    pattern = regex.sub(r'([^\])\*', pattern, '\1{' + star_len + '}')
-    pattern = regex.sub(r'([^\])\+', pattern, '\1{' + plus_len + '}')
-    return pattern
+    patt = regex.sub(r'([^\])\*', patt, '\1{' + star_len + '}')
+    patt = regex.sub(r'([^\])\+', patt, '\1{' + plus_len + '}')
+    return patt
 
 
-def exrex_pattern(pattern, star_len=10):
-    """ truncate_pattern(expand_pattern(pattern)) to help exrex pattern generator
+def exrex_pattern(patt, star_len=10):
+    r""" truncate_pattern(expand_pattern(patt)) to help exrex patt generator
 
-    >>> exrex_pattern(r'Hello [\w\s]*', 23)
-    'Hello [a-zA-Z_\n\r\t ]{23}'
+    >>> exrex_pattern(r'Hello\ [\w\s]*', 23)
+    'Hello\\ [a-zA-Z_\\t\\r\\n\ ]{23}'
     """
-    return truncate_pattern(expand_pattern(pattern))
+    return truncate_pattern(expand_pattern(patt), star_len=star_len)
 
 
-def regex_len(pattern):
-    """ Calculate the approximate minimum length of a str that will match the given regex (# states of FSM)
+def regex_len(patt):
+    r""" Calculate the approximate minimum length of a str that will match the given regex (# states of FSM)
 
     >>> 30 <= regex_len(r'([\w\s]{7}[\s]{1}\w?[0-9]{2})*', 3) <= 33)
     True
     """
-    pattern = getattr(pattern, 'pattern', pattern)
-    # return len(pattern)
-    return len(exrex.getone(exrex_pattern(pattern)))
+    patt = getattr(patt, 'pattern', patt)
+    # return len(patt)
+    return len(exrex.getone(exrex_pattern(patt)))
 
 
 RESPONSE_MAPPING = [('Hi', 'Hi!'), ("Hi", "Hi, I'm Bot")]
 CONTEXT = {}
 
 
-def compile_pattern(pattern, fuzziness=1, **kwargs):
-    """ Compile a Bot Language Pattern (glob star or AIML syntax) into a python regular experssion object
+def compile_pattern(patt, fuzziness=1, **kwargs):
+    r""" Compile a Bot Language Pattern (glob star or AIML syntax) into a python regular experssion object
 
     Args:
-      pattern (str): glob star pattern, regular expression pattern, or fixed string (exact match)
+      patt (str): glob star pattern, regular expression pattern, or fixed string (exact match)
 
     Returns:
-      compiled regex object if pattern contains _, +, *, #, or | characters
+      compiled regex object if patt contains _, +, *, #, or | characters
       str for fixed string patterns (only letters, spaces and natural English punctuation
 
     >>> compile_pattern("Hello|Hi *").match("Hi world")
     <regex.Match object; span=(0, 4), match='Hi w', fuzzy_counts=(1, 0, 0)>
     >>> compile_pattern("Hello World!")
-    'Hello World'
+    'Hello World!'
     """
     fuzziness = 3 if fuzziness is True else int(fuzziness) if fuzziness is not None else fuzziness
     if isinstance(fuzziness, float) and 0 < fuzziness < 1:
-        fuzziness = int(round(fuzziness * regex_len(pattern), 0))
-    if next(regex.finditer(r'[[*#{\\]', pattern), None):
-        # r'{' in pattern or r'[' in pattern or '\\' or r'*' in pattern or r'#' in pattern:
+        fuzziness = int(round(fuzziness * regex_len(patt), 0))
+    if next(regex.finditer(r'[[*#{\\]', patt), None):
+        # r'{' in patt or r'[' in patt or '\\' or r'*' in patt or r'#' in patt:
         if fuzziness:
-            pattern = '(' + pattern + '){e<=' + str(fuzziness) + '}'
-        if r'{' in pattern or r'[' in pattern or '\\' in pattern:
-            return regex.compile(pattern, **kwargs)
-        if r'*' in pattern or r'#' in pattern:
-            pattern.replace(r'*', r'[-a-zA-Z]+')
-            pattern.replace(r'#', r'([-a-zA-Z]+[ ]{0,1})+')
-            pattern.replace(r' ', r'[ ]')
-            pattern.replace(r'[[ ]]', r'[ ]')
-            return regex.compile(pattern, **kwargs)
+            patt = '(' + patt + '){e<=' + str(fuzziness) + '}'
+        if r'{' in patt or r'[' in patt or '\\' in patt:
+            return regex.compile(patt, **kwargs)
+        if r'*' in patt or r'#' in patt:
+            patt.replace(r'*', r'[-a-zA-Z]+')
+            patt.replace(r'#', r'([-a-zA-Z]+[ ]{0,1})+')
+            patt.replace(r' ', r'[ ]')
+            patt.replace(r'[[ ]]', r'[ ]')  # undo redundant brackets
+            patt.replace(r'[[ ]]', r'[ ]')  # undo tripply redundant brackets
+            return regex.compile(patt, **kwargs)
     else:
-        return pattern
+        return patt
+
+
+def remove_punc(s: str):
+    r""" Replace all nondigit-nonword characters with spaces and delete meaningless spaces
+
+    >>> remove_punc('<"hyphenated-word {variable}!!?">')
+    'hypenated word variable'
+    """
+    regex.sub(r'[^a-zA-Z0-9]', s, ' ').strip()
+    return s
 
 
 class PatternMap:
-    """ Like a dict, but with locality-sensitive hashes of keys, fuzzy regex keys, and redundant keys
+    r""" Like a dict, but with locality-sensitive hashes of keys, fuzzy regex keys, and redundant keys
 
     Methods:
       __getitem__(str): returns a list of str templates
@@ -165,20 +196,22 @@ class PatternMap:
         for pattern_template in mapping:
             if pattern_template is None:
                 continue
-            pattern, template = pattern_template[0], pattern_template[1]
-            pattern = compile_pattern(pattern)
-            if isinstance(pattern, str):
-                self.exact_strs[pattern] = self.exact_strs.get(pattern, []) + [template]
+            patt, template = pattern_template[0], pattern_template[1]
+            patt = compile_pattern(patt)
+            if isinstance(patt, str):
+                self.exact_strs[patt] = self.exact_strs.get(patt, []) + [template]
+                self.exact_strs[patt.lower()] = self.exact_strs.get(patt, []) + [template]
+                self.exact_strs[remove_punc(patt.lower())] = self.exact_strs.get(patt, []) + [template]
             else:
-                self.patterns[pattern.pattern] = (
-                    pattern, self.patterns.get(pattern.pattern, (pattern, []))[1] + [template])
+                self.patterns[patt.pattern] = (
+                    patt, self.patterns.get(patt.pattern, (patt, []))[1] + [template])
 
     def __getitem__(self, statement):
         """ Returns a list of possible resonse templates based on a statement==pattern match """
         if statement in self.exact_strs:
             return self.exact_strs[statement]
-        for (pattern_str, (pattern, templates)) in zip(self.patterns.keys(), self.patterns.values()):
-            if pattern.match(statement):
+        for (pattern_str, (patt, templates)) in zip(self.patterns.keys(), self.patterns.values()):
+            if patt.match(statement):
                 return templates
         return None
 
@@ -190,7 +223,7 @@ class PatternMap:
 
 
 class CallbackDict(dict):
-    """ A dictionary that will attempt to call the callback in value before returning the bare value
+    r""" A dictionary that will attempt to call the callback in value before returning the bare value
 
     References:
       https://stackoverflow.com/a/46106852/623735
@@ -210,41 +243,45 @@ class CallbackDict(dict):
 class Responder:
     """ See PatternMap class for a better approach """
 
-    def __init__(self, on_say=print, response_mapping=None, context=None):
+    def __init__(
+            self, on_say=print, response_mapping=None, context=None,
+            ignore_case=True, ignore_punc=True):
         self.response_mapping = response_mapping or RESPONSE_MAPPING
         self.on_say = on_say
         self.context = context or CONTEXT
+        self.ignore_case = ignore_case
+        self.ignore_punc = ignore_punc
 
     def say(self, s):
         self.on_say(s)
         return s
 
     def find_responses(self, statement):
-        """ Generating the respond from the bot
-        Args:
-            args([int], [int]) : the probabilities of choosing choice A as the respond or choice B as the respond
-        Returns:
-            returns([str]) : The bot respond
+        """ Return templates whose patterns match the provided statement (user utterance)
 
-        >>> respond('hi Bot',1,0)
-        'Hi! How can I help you.'
-        >>> respond('hi Bot',0,1)
-        'Hi user, How can I help you?'
+        Args:
+            statement (str): The user statement including capitalizaiton and punctuation
+        Returns:
+            [str]: The bot responses associated with the patterns that match the user statement
         """
         responses = []
         # O(N) algorithm must be improved with a pattern match search index or semantic search index like annoy
-        for pattern, template in self.response_mapping:
+        for patt, template in self.response_mapping:
             match = None
             try:
-                match = pattern.match(statement)
+                match = patt.match(statement)
             except AttributeError:
-                if pattern == statement:
+                if self.ignore_case:
+                    patt = patt.lower()
+                if self.ignore_punc:
+                    patt = remove_punc(patt)
+                if patt == statement:
                     match = True
             if match:
                 responses.append(template)
         return [r for (s, r) in self.response_mapping if s == statement]
 
-    def respond(self, statement, min_confidence):
+    def respond(self, statement, min_confidence=0):
         """ Reply with the best response that meets the min confidence threshold """
         responses = self.find_responses(statement)
         resp = responses[0]

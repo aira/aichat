5ae09d90718c0a06b17fc0ca819edd342894a4dd
Hobson Lane <github@totalgood.com>
2018-02-19 15:35:19 -0800

    fix test_aichat.py

--- tests/test_aichat.py
+++ tests/test_aichat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from aichat import chat
+from aichat import audio
 from aichat import constants
 from aichat import brain
 from aichat import context
@@ -15,14 +15,14 @@ __license__ = "mit"
 
 
 def test_respond():
-    assert chat.respond('hi') in (
+    assert brain.respond('hi') in (
         r"Hi! I'm Bot. How can I help you?",
         r"Hi! I'm Bot. I'm here to assist you. I can describe a scene by recognizing objects."
         )
 
 
-def test_chat():
-    assert doctest.testmod(chat, raise_on_error=True)
+def test_audio():
+    assert doctest.testmod(audio, raise_on_error=True)
 
 
 def test_constants():

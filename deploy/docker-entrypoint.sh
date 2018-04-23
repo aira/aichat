de1bc055473d4c5b4f92ca971ce1204f10de30dd
Hobson Lane <hobson@users.noreply.github.com>
2018-04-23 09:02:24 -0700

    modigfy docker

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+env
+conda info --envs
+source activate $CENV_NAME || echo "Unable to start conda env named $CENV_NAME"
 python manage.py migrate        # Apply database migrations
 mkdir -p static_root
 python manage.py collectstatic --clear --noinput # clear static files
@@ -6,16 +9,33 @@ python manage.py collectstatic --noinput  # collect static files
 # Prepare log files and start outputting logs to stdout
 touch /srv/logs/gunicorn.log
 touch /srv/logs/access.log
-tail -n 0 -f /srv/logs/*.log &
+# tail -n 0 -f /srv/logs/*.log &
 
-echo Starting nginx 
-# Start Gunicorn processes
-echo Starting Gunicorn.
-exec gunicorn aichat.wsgi:application \
+# # Start Gunicorn processes
+# echo Starting Gunicorn...
+# exec gunicorn aichat.wsgi:application \
+#     --name aichat_django \
+#     --bind 0.0.0.0:8000 \
+#     --workers 3 \
+#     --log-level=info \
+#     --log-file=/srv/logs/gunicorn.log \
+#     --access-logfile=/srv/logs/access.log &
+
+# echo Starting nginx...
+# exec service nginx start
+
+echo 'Startings gunicorn and nginx logs...'
+tmux aichat-session -d 'tail -n 0 -f /srv/logs/*.log'
+
+echo 'starting gunicorn'
+tmux new-window -v 'exec gunicorn aichat.wsgi:application \
     --name aichat_django \
     --bind 0.0.0.0:8000 \
     --workers 3 \
     --log-level=info \
     --log-file=/srv/logs/gunicorn.log \
-    --access-logfile=/srv/logs/access.log "$@" & \
-exec service nginx start
+    --access-logfile=/srv/logs/access.log &'
+
+echo Starting nginx...
+tmux new-window 'exec service nginx start'
+tmux -2 attach-session -d 

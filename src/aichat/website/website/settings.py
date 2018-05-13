6d568d332232b6a64fdc6b5f2954b0a683574284
Hobson Lane <hobson@users.noreply.github.com>
2018-05-13 13:17:54 -0400

    move website package to aichat.website so will be built with setup.py

--- src/aichat/website/website/settings.py
+++ src/aichat/website/website/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
-from .local_settings import SECRET_KEY  # noqa
+from .secret_settings import SECRET_KEY  # noqa
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

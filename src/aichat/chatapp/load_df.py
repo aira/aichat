c78efc35412acb9b634034100b6eab8a534d75c9
RigobertoM29 <rigomacedo0629@gmail.com>
2018-06-08 15:15:53 -0700

    fixed twopatt in gen_links

--- src/aichat/chatapp/load_df.py
+++ src/aichat/chatapp/load_df.py
@@ -18,12 +18,17 @@ def load_df(path=DATA_DIR):
         """
     CSV_COLUMNS = ('trigger', 'response', 'source_state', 'dest_state')
     df = pd.DataFrame(columns=CSV_COLUMNS)
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if file.endswith(".csv"):
-                dfadd = pd.read_csv(path + '/' + file)
-                dfadd.columns = CSV_COLUMNS[:len(dfadd.columns)]
-                df = df.append(dfadd)
+    if os.path.isfile(path):
+        dfadd = pd.read_csv(path)
+        dfadd.columns = CSV_COLUMNS[:len(dfadd.columns)]
+        df = df.append(dfadd)
+    else:
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if file.endswith(".csv"):
+                    dfadd = pd.read_csv(path + '/' + file)
+                    dfadd.columns = CSV_COLUMNS[:len(dfadd.columns)]
+                    df = df.append(dfadd)
 
     df_clean = pd.DataFrame(columns=CSV_COLUMNS, index=None)
     for column in CSV_COLUMNS:
@@ -36,11 +41,13 @@ def load_df(path=DATA_DIR):
 
 DF = load_df()
 
+# gen_nodes returns list of nodes for the value of the base d3 'nodes': json
 
-def nodes_to_list(path=DATA_DIR, DF=DF):
+
+def gen_nodes(path=DATA_DIR, DF=DF):
     """ returns list of all fixed non pattern nodes names
 
-    >>> sorted([sorted(list(d.items())) for d in nodes_to_list()])
+    >>> sorted([sorted(list(d.items())) for d in gen_nodes()])
     [[('id', 'node0'), ('name', 'Bluetooth connecting headset')],
      [('id', 'node1'), ('name', 'Bluetooth connecting paired')],
      [('id', 'node10'), ('name', 'Chloe Skill Quickstart')],
@@ -76,39 +83,7 @@ def nodes_to_list(path=DATA_DIR, DF=DF):
         node_index = node_index + 1
     return dict_list
 
-
-def links_to_list(path=DATA_DIR, value=1):
-
-    links = []
-    node_names = list(set(DF.dest_state))
-    for i, source_pattern in enumerate(DF.source_state.values):
-        patt = pattern.expand_globstar(source_pattern)
-        for j, name in enumerate(node_names):
-            if ('{' in patt or '[' in patt) and regex.match(patt, name):
-                print(str(j) + source_pattern + ',' + name)
-                if ('*' in name or '?' in name):
-                    pass
-                else:
-                    links.append({'source': node_names.index(name),
-                                  'target': node_names.index(DF.dest_state.values[i]),
-                                  'command': DF.trigger[i],
-                                  'response': DF.response[i],
-                                  'value': value})
-            elif source_pattern == name:
-                if ('*' in name or '?' in name):
-                    pass
-                else:
-                    print("nonpattern:" + str(j) + source_pattern + ',' + name)
-                    links.append({'source': node_names.index(name),
-                                  'target': node_names.index(DF.dest_state.values[i]),
-                                  'command': DF.trigger[i],
-                                  'response': DF.response[i],
-                                  'value': value})
-    return links
-
-
-def node_filter(pattern, names=nodes_to_list()):
-    return [name for name in names if regex.match(pattern, name)]
+# is glob_star returns true if node is a golbstar
 
 
 def is_globstar(node_name):
@@ -123,45 +98,14 @@ def get_nodes(path=DATA_DIR, DF=DF):
     return [node for node in node_list if '*' not in node and '?' not in node and '|' not in node]
 
 
-def links_to_list2(path=DATA_DIR, value=1):
-    links = []
-    nonpattern_node_names = nodes_to_list()
-    node_names = list(set(DF.dest_state))
-    for i, source_pattern in enumerate(DF.source_state.values):
-        source_patt = pattern.expand_globstar(source_pattern)
-        for j, dest_pattern in enumerate(node_names):
-            dest_patt = pattern.expand_globstar(dest_pattern)
-            if ('{' in source_patt or '[' in source_patt) and regex.match(source_patt, source_pattern):
-                print(str(j) + source_pattern + ',' + dest_pattern)
-                if ('{' in dest_patt or '[' in dest_patt) and regex.match(dest_patt, dest_pattern):
-                    print(str(j) + dest_patt + ',' + dest_pattern)
-                    for dest_node in nonpattern_node_names:
-                        links.append({'source': nonpattern_node_names.index(source_pattern),
-                                      'target': nonpattern_node_names.index(DF.dest_node.values[i]),
-                                      'command': DF.trigger[i],
-                                      'response': DF.response[i],
-                                      'value': value})
-                else:
-                    links.append({'source': nonpattern_node_names.index(source_pattern),
-                                  'target': nonpattern_node_names.index(DF.dest_state.values[j]),
-                                  'command': DF.trigger[i],
-                                  'response': DF.response[i],
-                                  'value': value})
-            elif source_pattern == dest_pattern:
-                if ('*' in dest_pattern or '?' in dest_pattern):
-                    pass
-                else:
-                    print("nonpattern:" + str(j) + source_pattern + ',' + dest_pattern)
-                    links.append({'source': nonpattern_node_names.index(dest_pattern),
-                                  'target': nonpattern_node_names.index(dest_pattern),
-                                  'command': DF.trigger[i],
-                                  'response': DF.response[i],
-                                  'value': value})
-    return links
+def node_filter(pattern, names=get_nodes()):
+    return [name for name in names if regex.match(pattern, name)]
+
+# gen_nodes returns list of links for the value of the base d3 'links': json
 
 
 def gen_links(path=DATA_DIR, value=1, df=DF):
-    """ returns a d3 graph datastructure
+    """ returns links for the d3 structure
 
     # Case1 nopatt
     >>> df = pd.DataFrame([['1 goto 2', 'okay', '1', '2']], columns=('trigger', 'response', 'source_state', 'dest_state'))
@@ -169,12 +113,12 @@ def gen_links(path=DATA_DIR, value=1, df=DF):
     [{'source': 0, 'target': 1, 'command': '1 goto 2', 'response': 'okay', 'value': 1}]
 
     # Case3 destpatt
-    >>> df = pd.DataFrame([['', '', '1', '2'], ['', '', '2', '3'], ['', '', '3', '4'], ['', '', '4', '1|2']], columns=('trigger', 'response', 'source_state', 'dest_state')) # noqa
+    >>> df = pd.DataFrame([['1 goto 2', 'okay', '1', '2'], ['2 goto 3', 'okay', '2', '3'], ['3 goto 4', 'okay', '3', '4'], ['4 goto 1 or 2', 'okay', '4', '1|2']], columns=('trigger', 'response', 'source_state', 'dest_state')) # noqa
     >>> gen_links(path=DATA_DIR, value=1, df=df)
-    [{'source': 0, 'target': 1, 'command': '', 'response': '', 'value': 1},
-     {'source': 1, 'target': 2, 'command': '', 'response': '', 'value': 1},
-     {'source': 2, 'target': 3, 'command': '', 'response': '', 'value': 1},
-     {'source': 3, 'target': 1, 'command': '', 'response': '', 'value': 1}]
+    [{'source': 0, 'target': 1, 'command': '1 goto 2', 'response': 'okay', 'value': 1},
+     {'source': 1, 'target': 2, 'command': '2 goto 3', 'response': 'okay', 'value': 1},
+     {'source': 2, 'target': 3, 'command': '3 goto 4', 'response': 'okay', 'value': 1},
+     {'source': 3, 'target': 1, 'command': '4 goto 1 or 2', 'response': 'okay', 'value': 1}]
 
     # Case2 sourcepatt
     >>> df = pd.DataFrame([['', '', '1', '2'], ['', '', '2', '3'], ['', '', '3', '4'], ['', '', '1|2', '5']], columns=('trigger', 'response', 'source_state', 'dest_state')) # noqa
@@ -230,7 +174,7 @@ def gen_links(path=DATA_DIR, value=1, df=DF):
                         links.append({'source': node_names.index(source_name),
                                       'target': node_names.index(dest_name),
                                       'command': df.trigger[source_index],
-                                      'response': df.response[source_index],
+                                      'response': df.response[dest_index],
                                       'value': value})
         else:
             if not is_globstar(df.dest_state[source_index]):
@@ -247,9 +191,9 @@ def gen_links(path=DATA_DIR, value=1, df=DF):
                 source_pattern = pattern.expand_globstar(source_name)
                 current_dest_name = df.dest_state[source_index]
                 dest_pattern = pattern.expand_globstar(current_dest_name)
-                for source_index, current_source_name in enumerate(df.source_state.values):
+                for current_source_index, current_source_name in enumerate(node_names):
                     if regex.match(source_pattern, current_source_name) and not is_globstar(current_source_name):
-                        for dest_index, dest_name in enumerate(df.dest_state.values):
+                        for dest_index, dest_name in enumerate(node_names):
                             if regex.match(dest_pattern, dest_name) and not is_globstar(dest_name):
                                 links.append({'source': node_names.index(current_source_name),
                                               'target': node_names.index(dest_name),
@@ -261,8 +205,8 @@ def gen_links(path=DATA_DIR, value=1, df=DF):
 
 def create_json(path=DATA_DIR):
     dialog = dialog_graph.base_dialog()
-    dialog['nodes'] = nodes_to_list()
-    dialog['links'] = links_to_list()
+    dialog['nodes'] = gen_nodes()
+    dialog['links'] = gen_links()
     js = json.dumps(dialog, indent=2)
     with open("../data/newdialog.json", "w") as f:
         f.write(js)

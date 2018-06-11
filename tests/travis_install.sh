eb755626d1e1260e6e2f46ae387edce95667c78a
Hobson Lane <hobson@users.noreply.github.com>
2018-06-11 11:20:56 -0700

    use working anaconda3 script from NLPIA

--- tests/travis_install.sh
+++ tests/travis_install.sh
@@ -56,87 +56,61 @@ elif [ $UNVERSIONED_OS == "linux" ] ; then
     # apt-get install -y nginx supervisor sqlite3 
 fi
 
-if [[ "$DISTRIB" == "conda" ]] ; then
-    # Deactivate the travis-provided virtual environment and setup a conda-based environment instead
-    deactivate || echo "no virtualenv has been activated yet (NOT running on a travis container)"
+set -e
 
+if [[ "$DISTRIB" == "conda" ]]; then
+    # Deactivate the travis-provided virtual environment and setup a
+    # conda-based environment instead
+    deactivate
 
     # Use the anaconda3 installer
+    DOWNLOAD_DIR=${DOWNLOAD_DIR:-$HOME/.tmp/anaconda3}
+    mkdir -p $DOWNLOAD_DIR
+    wget http://repo.continuum.io/archive/Anaconda3-5.1.0-Linux-x86_64.sh -O $DOWNLOAD_DIR/anaconda3.sh
+    chmod +x $DOWNLOAD_DIR/anaconda3.sh && bash $DOWNLOAD_DIR/anaconda3.sh -b -u -p $HOME/anaconda3
+    # rm -r -d -f $DOWNLOAD_DIR
+    export PATH=$HOME/anaconda3/bin:$PATH
+    conda update -y conda
+    conda install -y pip
+    conda install -y swig
 
-    if [[ -f "$DOWNLOAD_DIR/anaconda3.sh" || -f "$HOME/anaconda3/bin"  ]] ; then
-        echo $(ls -hal $DOWNLOAD_DIR)
+    # Configure the conda environment and put it in the path using the provided versions
+    if [[ -f "$ENVIRONMENT_YML" ]]; then
+        conda env create -n testenv -f "$ENVIRONMENT_YML"
     else
-        wget http://repo.continuum.io/archive/Anaconda3-5.1.0-Linux-x86_64.sh -O $DOWNLOAD_DIR/anaconda3.sh
-        chmod +x $DOWNLOAD_DIR/anaconda3.sh
+        echo "WARNING: Unable to find an environment.yml file !!!!!!"
+        conda create -n testenv --yes python=$PYTHON_VERSION pip
     fi
-
-    if [[ -f "$HOME/anaconda3/bin"  ]] ; then
-        echo $(ls -hal "$HOME/anaconda3")
-    else
-        bash $DOWNLOAD_DIR/anaconda3.sh -b -u -p $HOME/anaconda3
-	    export PATH=$HOME/anaconda3/bin:$PATH
-	    conda update -y conda
-	    conda install -y pip swig nltk
-
-	    # Configure the conda environment and put it in the path using the provided versions
-	    if [[ -f "$ENVIRONMENT_YML" ]]; then
-	        conda env create -n $CENV_NAME -f "$ENVIRONMENT_YML" || echo "conda env $CENV_NAME already exists"
-	    else
-	        echo "WARNING: Unable to find an environment.yml file !!!!!!"
-	        conda create -n $CENV_NAME --yes python=$PYTHON_VERSION pip
-	    fi
-    fi
-
-    source activate $CENV_NAME
-    conda install -y pip swig nltk
-    pip install --upgrade pip
-
-    if [[ -f "$REQUIREMENTS_PATH" ]] ; then
-    	echo "Installing requirements file $REQUIREMENTS_PATH from PWD=$PWD"
-        pip install -r "$REQUIREMENTS_PATH"
-   	else
-        echo "Unable to find requirements file $REQUIREMENTS_PATH from PWD=$PWD"
-        echo "Currently working directory: $PWD"
-        echo "ls:"
-        ls
-    fi
+    source activate testenv
+    conda install -y pip
 
     # download spacy English language model
-    pip install spacy
-	python -m spacy download en
+    pip install --upgrade spacy
+    python -m spacy download en
 
     # download NLTK punkt, Penn Treebank, and wordnet corpora 
-    pip install nltk
     python -c "import nltk; nltk.download('punkt'); nltk.download('treebank'); nltk.download('wordnet');"
     which python
     python --version
 
-    # if [[ "$INSTALL_MKL" == "true" ]]; then
-    #     TO_INSTALL="$TO_INSTALL -c anaconda mkl"
-    # else
-    #     TO_INSTALL="$TO_INSTALL -c anaconda nomkl"
-    # fi
-
-    # if [[ -n "$PANDAS_VERSION" ]]; then
-    #     TO_INSTALL="$TO_INSTALL -c anaconda pandas=$PANDAS_VERSION"
-    # fi
-
-    # if [[ -n "$PYAMG_VERSION" ]]; then
-    #     TO_INSTALL="$TO_INSTALL -c anaconda pyamg=$PYAMG_VERSION"
-    # fi
-
-    # if [[ -n "$PILLOW_VERSION" ]]; then
-    #     TO_INSTALL="$TO_INSTALL -c anaconda pillow=$PILLOW_VERSION"
-    # fi
-
-    # conda create -n $CENV_NAME --yes $TO_INSTALL
-
-    echo "conda list: $(conda list)"
-    echo "pip freeze: $(pip freeze)"
-
 elif [[ "$DISTRIB" == "ubuntu" ]]; then
     # Use standard ubuntu packages in their default version
     echo $DISTRIB
+    apt-get install -y build-essential swig gfortran
+    apt-get install -y python-dev python3-dev python-pip python3-pip 
+
+    apt-get install -y python-igraph
+
+    # SpeechRecognizer requires PyAudio
+    apt-get install -y portaudio19-dev python-pyaudio python3-pyaudio
+
+    # for scipy
+    apt-get install -y libopenblas-dev liblapack-dev
+    apt-get install -y python-scipy python3-scipy
+
+    # for matplotlib:
+    apt-get install -y libpng12-dev libfreetype6-dev
+    apt-get install -y tcl-dev tk-dev python-tk python3-tk
 fi
 
 if [[ "$COVERAGE" == "true" ]]; then
